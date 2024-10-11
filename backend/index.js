88be740d2933cd11ffe3af0a4dce250af8950967
Shashank <shashank_@outlook.in>
2024-10-11 14:26:16 +0530

    .

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 // Configure multer to store files in memory
